c7dabd126c7e292eda5cc9ddeba0923db263314a
gracebir <grace.devolop@gmail.com>
2022-12-21 18:50:28 +0200

    adding validation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,50 +1,59 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-
+import { faEye, faEyeSlash,  } from "@fortawesome/free-solid-svg-icons";
+import { useFormik } from 'formik'
 import "../sign-up-form/SignUpForm.scss";
 import Input, { Type } from "../Input";
+import { BiErrorCircle} from 'react-icons/bi'
+import { SignInSchema } from "../../validateSchema";
 
 const LoginForm = () => {
-    const [login, setLogin] = useState({
-        userId: { name: "userId", value: "" },
-        password: { name: "password", value: "" },
-    });
     const [passwordVisible, setPasswordVisible] = useState(false);
-
+    const [isError, setIsError] = useState(false);
+    const onSubmit = (values : any)=>{
+        console.log(values)
+        setIsError(false)
+    }
+    const { values, handleChange, handleSubmit,handleBlur} = useFormik({
+        initialValues: {
+            userId:'',
+            password: ''
+        },
+        onSubmit,
+        validationSchema: SignInSchema
+    })
     return (
         <div className="registration-Div">
-            <form className="registration-form">
+                <form
+                onSubmit={handleSubmit}
+                  className="registration-form">
                 <header>
                     <h2 className="registration-header">Sign In
                     </h2>
                     <h4 className="registration-header">
-                        Don have an account yet? <a href="registrationPage">Register</a>
+                        Don't have an account yet? <a href="registrationPage">Register</a>
                     </h4>
                 </header>
-                {/* <div>
+                <div className={isError ?"error" : "class-error"}>
+                    <BiErrorCircle/>
                     <p>Wrong User info please check</p>
-                </div> */}
+                </div>
                 <div className="registration-info">
                     <Input
-                        name={login.userId.name}
+                        name='userId'
                         placeholder="User ID"
+                        onBlur={handleBlur}
+                        value={values.userId}
+                        onChange={handleChange}
                     />
                     <div className="field-container">
                         <Input
-                            name={login.password.name}
+                            name='password'
                             type={passwordVisible ? Type.text : Type.password}
                             placeholder="min. 6 charcters"
-                            minLength={6}
-                            onChange={(e) => {
-                                setLogin((prev) => ({
-                                    ...prev,
-                                    password: {
-                                        ...login.password,
-                                        value: e.target.value,
-                                    },
-                                }));
-                            }}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            onChange={handleChange}
                         />
                         <span
                             title={passwordVisible ? "hide password" : "show password"}
@@ -61,7 +70,7 @@ const LoginForm = () => {
                         <a href="/hierarchy">Forget Password</a>
                     </div>
                     <div className="register-button">
-                        <button>Sign In</button>
+                        <button type="submit">Sign In</button>
                     </div>
                 </div>
             </form>
