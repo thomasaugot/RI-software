95f4f649bd2086d9063ef0ff4b3bcba8b4a7ea87
gracebir <grace.devolop@gmail.com>
2022-12-22 17:50:05 +0200

    adding ref to password

--- src/validateSchema/index.tsx
+++ src/validateSchema/index.tsx
@@ -6,7 +6,7 @@ export const SignupSchema = yup.object().shape({
     login: yup.string().required("Required"),
     email: yup.string().email("please enter a valid email").required("Required"),
     password: yup.string().min(6).required("required"),
-    confirmPassword: yup.string().required("Required").nullable(),
+    confirmPassword: yup.string().oneOf([yup.ref("password"),null], "Passwords doesn't much").required("Required").nullable(),
     phoneNumber: yup.string().required("Required").nullable(),
 });
 
