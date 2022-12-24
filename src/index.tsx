71aa7133fd1017e1beb3c445c35d3b06f56b4485
gracebir <grace.devolop@gmail.com>
2022-12-24 16:14:03 +0200

    fixing something on login validation

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { QueryClientProvider as QueryProvider, QueryClient } from 'react-query'
 import "./index.scss";
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryProvider client={queryClient}>
+      <App />
+    </QueryProvider>
   </React.StrictMode>
 );
