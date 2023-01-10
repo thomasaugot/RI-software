88d20df67d1a7383a8ab878248182359d1c2353c
gracebir <grace.devolop@gmail.com>
2023-01-10 15:06:54 +0200

    adding login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </>
   );
 }
