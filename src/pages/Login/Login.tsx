b76cb5bb20bf9d3cab8acb1c949d30f92bb9f3a3
gracebir <grace.devolop@gmail.com>
2023-01-16 18:32:14 +0200

    formatting document

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,65 +1,57 @@
-import  { useState } from "react";
-import { Link, Navigate } from "react-router-dom";
-import { BiErrorCircle } from 'react-icons/bi'
-import {eye, eyeoff} from '../../assets/Icons'
+import { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { BiErrorCircle } from "react-icons/bi";
+import { eye, eyeoff } from "../../assets/Icons";
 import CheckBox from "../../components/CheckBox/CheckBox";
 import Field from "../../components/InputField/InputField";
 import Heading from "../../components/Title/Title";
-import Submitbutton from "../../components/SubmitButton/SubmitButton";
+import SubmitButton from "../../components/SubmitButton/SubmitButton";
 import Text from "../../components/Text/Text";
 import "./Login.scss";
 import { buttonType } from "../../types";
-
-const baseURl = process.env.REACT_APP_URL;
+import { login } from "../../queries";
 
 const Login = () => {
   const [isError, setIsError] = useState(false);
-  const [isVisible, setIsVisible] = useState(false)
+  const [isVisible, setIsVisible] = useState(false);
   const [errorText, setErrorText] = useState("");
   const [isChecked, setIsChecked] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
   // login method
-  const onSumit = async (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    let data = {
-      email,
-      password
-    }
-    return await  fetch(`${baseURl}/api/login`,{
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
-    })
-    .then((res) => {
-      if(res.status === 400){
-        setIsError(true)
-        setErrorText("Wrong email or password")
-      } else if(res.status === 200){
-        setIsError(false)
-        if(isChecked){
-          localStorage.setItem("isLogged", "true")
+  const onSumit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    return await login({ email, password })
+      .then((res) => {
+        if (res.status === 400) {
+          setIsError(true);
+          setErrorText("Wrong email or password");
+        } else if (res.status === 200) {
+          setIsError(false);
+          if (isChecked) {
+            localStorage.setItem("isLogged", "true");
+          }
+          console.log("isLogged");
+          navigate("/");
         }
-        console.log("isLogged")
-      }
-      return res.json()
-    }
-    )
-    .then((data) => localStorage.setItem("token", data.result.access_token))
-    .catch((err)=> {
-      setIsError(true) 
-    })
-  }
+        return res.json();
+      })
+      .then((data) => localStorage.setItem("token", data.result.access_token))
+      .catch((err) => {
+        setIsError(true);
+      });
+  };
 
-  if(localStorage.getItem("isLogged")==="true") return <Navigate to='/' replace/>
+  if (localStorage.getItem("isLogged") === "true")
+    return <Navigate to="/" replace />;
 
   return (
     <div className="login">
-      <div className="login__container">
-        <div className="login__header">
+      <div className="login-container">
+        <div className="login-header">
           <Heading text="Sign In" />
-          <div className="login__text">
+          <div className="login-text">
             <Text text="Don’t have an account yet?" />
             <Link to="/registration">
               <Text text=" Register" color="#0B8FEF" />
@@ -67,16 +59,28 @@ const Login = () => {
           </div>
         </div>
         {isError ? (
-          <div className="login__error">
-            <BiErrorCircle size={"1.25rem"}/>
+          <div className="login-error">
+            <BiErrorCircle size={"1.25rem"} />
             <Text color="#F61D1D" text={errorText} />
           </div>
         ) : null}
-        <form onSubmit={onSumit} className="login__form">
+        <form onSubmit={onSumit} className="login-form">
           <div className="form-controls">
-            <Field type="email" onChange={(e)=> setEmail(e.target.value)} value={email} placeholder="Email" name="email" />
-            <div className="password_control">
-              <Field type={isVisible ? "text": "password"} onChange={(e)=> setPassword(e.target.value)} value={password} placeholder="Password" name="password" />
+            <Field
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder="Email"
+              name="email"
+            />
+            <div className="password-control">
+              <Field
+                type={isVisible ? "text" : "password"}
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder="Password"
+                name="password"
+              />
               <span
                 title={isVisible ? "hide password" : "show password"}
                 className="input-icon"
@@ -84,15 +88,21 @@ const Login = () => {
                   setIsVisible(!isVisible);
                 }}
               >
-             {isVisible ? eye : eyeoff}
-           </span>
+                {isVisible ? eye : eyeoff}
+              </span>
             </div>
           </div>
           <div className="login__options">
-            <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} text="Remember me"/>
-            <Link to="/forget" className="login__forget">Forget password?</Link>
+            <CheckBox
+              isChecked={isChecked}
+              setIsChecked={setIsChecked}
+              text="Remember me"
+            />
+            <Link to="/forget" className="login__forget">
+              Forget password?
+            </Link>
           </div>
-          <Submitbutton type={buttonType.submit} text="Sign In" />
+          <SubmitButton type={buttonType.submit} text="Sign In" />
         </form>
       </div>
     </div>
