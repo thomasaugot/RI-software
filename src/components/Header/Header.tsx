a98718d43f552269d770612374791c55648f5a1d
gracebir <grace.devolop@gmail.com>
2023-01-19 00:33:22 +0200

    creating header components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.scss'
+
+function Header() {
+  return (
+    <div className='header-container'>
+      <img src="https://tinypic.host/images/2023/01/18/Group-215.png" alt="" />
+    </div>
+  )
+}
+
+export default Header
