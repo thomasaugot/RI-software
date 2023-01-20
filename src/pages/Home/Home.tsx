5f4ee1f0caad3931270a7f8f757a67f4fe6ce988
gracebir <grace.devolop@gmail.com>
2023-01-20 16:03:50 +0200

    Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BaseLayout from '../../layouts/BaseLayout'
+
+function Home() {
+  return (
+    <BaseLayout>
+      HomePage
+    </BaseLayout>
+  )
+}
+
+export default Home
