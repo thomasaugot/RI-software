29ec13653980ab2ddfb5ea2e1e9fecbbd1940556
gracebir <grace.devolop@gmail.com>
2023-01-21 15:58:03 +0200

    run pretty format to routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,14 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import Confirmation from "./pages/Confirmation/Confirmation";
-import Login from './pages/Login/Login'
+import Login from "./pages/Login/Login";
 import SingUpForm from "./pages/SignUp/SignUp";
 import Home from "./pages/Home/Home";
 
 const Router: FC = () => {
   return (
     <Routes>
-        <Route path="/login" element={<Login/>}/>
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<SingUpForm />} />
       <Route path="/confirm" element={<Confirmation />} />
       <Route path="/" element={<Home />} />
