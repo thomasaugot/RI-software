3c560bd8f73ba5eab92d87dcac55b780f3cfc2c6
gracebir <grace.devolop@gmail.com>
2023-01-22 01:05:21 +0200

    navbar icons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,43 +1,49 @@
-import React from 'react'
-import './NavBar.scss'
-import { navlogo, Logout } from '../../assets/Icons'
-import NavBarItem from './NavBarItem/NavBarItem'
-import { navbars } from '../../queries'
-import { navbarProps } from '../../types/types'
+import React from "react";
+import "./NavBar.scss";
+import {
+  navlogo,
+  Logout,
+} from "../../assets/Icons";
+import NavBarItem from "./NavBarItem/NavBarItem";
+import { navbars } from "../../queries";
+import { navbarProps } from "../../types/types";
 
 function NavBar() {
-  const [navData, setNavData] = React.useState<Array<navbarProps>>([])
-
+  const [navData, setNavData] = React.useState<Array<navbarProps>>([]);
 
   React.useEffect(() => {
     const getNavData = async () => {
-      const datas = await navbars(1)
-      setNavData(datas)
-    }
-    getNavData()
-  }, [])
+      const datas = await navbars(1);
+      setNavData(datas);
+    };
+    getNavData();
+  }, []);
 
-  console.log(navData)
+  console.log(navData);
   return (
-    <div className='navbar'>
-        <div className="navbar-header">
-            <span>{navlogo}</span>
-            <p className='navbar-head-text'>Store Panel</p>
-        </div>
+    <div className="navbar">
+      <div className="navbar-header">
+        <span>{navlogo}</span>
+        <p className="navbar-head-text">Store Panel</p>
+      </div>
       <div className="navbar-container">
         {navData.map((item, i) => (
-          <NavBarItem key={i} text={item.name} menuItems={item.subitems} index={i + 1} />
+          <NavBarItem
+            key={i}
+            text={item.name}
+            menuItems={item.subitems}
+            index={i + 1}
+          />
         ))}
       </div>
       <div className="navbar-bottom">
-        <div className='logout-button'>
+        <div className="logout-button">
           <span>{Logout}</span>
           <p>Logout</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
-
+export default NavBar;
--- src/components/NavBar/NavBarItem/NavBarItem.tsx
+++ src/components/NavBar/NavBarItem/NavBarItem.tsx
@@ -1,35 +1,50 @@
-import React from 'react'
-import './NavBarItem.scss'
-import {FiChevronRight} from 'react-icons/fi'
-import { Link } from 'react-router-dom'
-import { NavItemProps } from '../../../types/types'
+import React from "react";
+import "./NavBarItem.scss";
+import {
+  employee,
+  project,
+  sale,
+  purchase,
+  finance
+} from "../../../assets/Icons";
+import { FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { NavItemProps } from "../../../types/types";
 
-function NavBarItem({index, text,icon, menuItems}: NavItemProps) {
-    const [isOpen, setIsOpen] = React.useState(false)
-    const onOpen =() => {
-        if(index){
-            setIsOpen(!isOpen)
-        }
+const navIcons = [employee, project, sale, purchase, finance];
+
+function NavBarItem({ index, text, menuItems }: NavItemProps) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onOpen = () => {
+    if (index) {
+      setIsOpen(!isOpen);
     }
+  };
+  console.log("index",(index!)-1);
   return (
-    <div onClick={onOpen} className={isOpen ? 'navitem-expand':'navitem'}>
-        <div className='nav-item-top'>
-            <div className='nav-item-top-text'>
-                <span className='icon'>{icon}</span>
-                <p>{text}</p>
-            </div>
-            <FiChevronRight className={isOpen? 'rotate': 'rotate-0'}/>
+    <div onClick={onOpen} className={isOpen ? "navitem-expand" : "navitem"}>
+      <div className="nav-item-top">
+        <div className="nav-item-top-text">
+          <span className="icons">{navIcons[(index!)-1]}</span>
+          <p>{text}</p>
         </div>
-        <div className={isOpen ? 'nav-item-menu': 'hide'}>
-           {menuItems?.map((item, i)=>(
-            <Link className='nav-menu-link' onClick={()=>setIsOpen(false)} to={""} key={i}>
-                <span>{item.icon}</span>
-                <p>{item.name}</p>
-            </Link>
-           ))}
-        </div>
+        <FiChevronRight className={isOpen ? "rotate" : "rotate-0"} />
+      </div>
+      <div className={isOpen ? "nav-item-menu" : "hide"}>
+        {menuItems?.map((item, i) => (
+          <Link
+            className="nav-menu-link"
+            onClick={() => setIsOpen(false)}
+            to={""}
+            key={i}
+          >
+            <span>{item.icon}</span>
+            <p>{item.name}</p>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBarItem
+export default NavBarItem;
