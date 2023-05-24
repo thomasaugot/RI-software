0af35bb45a4e3fdfbe86a80a6add2eed9233e164
Thomas Augot <thomas.augot@hotmail.fr>
2023-05-24 09:33:00 +0200

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ModalsProvider from './context/modalsContext'
-import ChatProvider from './context/chat/chatContext';
-import ProfileProvider from './context/profile/profileContext';
-import ProjectProvider from './context/project/projectContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import ModalsProvider from "./context/modalsContext";
+import ChatProvider from "./context/chat/chatContext";
+import ProfileProvider from "./context/profile/profileContext";
+import ProjectProvider from "./context/project/projectContext";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <ModalsProvider>
     <ChatProvider>
